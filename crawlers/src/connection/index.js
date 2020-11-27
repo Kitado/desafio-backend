459dafc610c34bd3d4114b123c956bc4f6112835
victorhugol <victorsilva698@gmail.com>
2020-11-27 12:35:10 -0300

    Pegando infos do site da kabum e convertendo/tratando moeda referente aos produtos

--- crawlers/src/connection/index.js
+++ crawlers/src/connection/index.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
 
 
+
+
+
+mongoose.connect(
+  `mongodb://nodeauth:nodeauth@localhost:27017/nodeauth?authSource=admin`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+mongoose.connection.on('error', () => console.error('connection error:'))
+mongoose.connection.once('open', () => console.log('database connected'))
+
+
+
+module.exports = mongoose;
+
