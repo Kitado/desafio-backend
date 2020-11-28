a972220f0ed60483708e66e9c39315bc47f215c5
victorhugol <victorsilva698@gmail.com>
2020-11-28 15:46:52 -0300

    buscando e guardando os produtos

--- crawlers/src/index.js
+++ crawlers/src/index.js
@@ -1,5 +1,14 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+// const appExpress = express();
+// appExpress(express.json());
 
+
+// appExpress.get('/',(req,res)=>{
+//     return res.send({ola:"mundo"})
+// })
+
+// appExpress.listen(3001)
 
 const Crawler = require('./entities/Crawler');
 
@@ -10,11 +19,24 @@ const Crawler = require('./entities/Crawler');
     const page = await Browser.newPage();
     await page.goto("https://www.kabum.com.br/");
     const app = new Crawler(Browser,page);
-    app.buscaProduto('placa de video');
-    await page.waitForNavigation();
-    console.log(await app.infoProduto());
-    app.buscaProduto('HD SSD');
-    await page.waitForNavigation();
-    console.log(await app.infoProduto());
+    // 
+
+    let produto = await app.buscaProduto('placa de video');
+    await app.cadastraProduto(produto);
+
+    produto = await app.buscaProduto('teclado');
+    await app.cadastraProduto(produto);
+
+    produto = await app.buscaProduto('mouse');
+    await app.cadastraProduto(produto);
+
+    produto = await app.buscaProduto('HD SSD');
+    await app.cadastraProduto(produto);
+
+    produto = await app.buscaProduto('monitor');
+    await app.cadastraProduto(produto);
+
+    produto = await app.buscaProduto('gabinete');
+    await app.cadastraProduto(produto);
 
 })();
