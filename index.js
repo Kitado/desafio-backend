df2a3d0cfa47a501b45613b39d5180b43b8e4238
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2020-11-28 15:28:36 -0300

    Iniciando repositório
    Iniciando o Web Crawler, já com a funcionalidade de pegar dados sobre teclados.

---
+++ index.js
@@ -0,0 +1,47 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const fecthData = async(url) =>{
+    try{
+        const result = await axios.get(url)
+        return result.data
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+const keyboard = async() =>{
+    const content = await fecthData('https://www.pichau.com.br/perifericos/teclado')
+    const products = []
+    const $ = cheerio.load(content)
+
+    axios.get('https://economia.awesomeapi.com.br/json/all').then(dados =>{
+        let dolar = dados.data.USD.high
+    
+
+    $('li.product-item').each((i, e) =>{
+        const title = $(e).find('a.product-item-link').text()
+        const link =  $(e).find('a.product-item-link').attr('href')
+        const linkImg = $(e).find('img.product-image-photo').attr('src')
+        const preco = $(e).find('span.price-boleto').text().slice(1)
+        let finalPrice = preco.split(' ')
+        let formatedPrice = parseFloat(finalPrice[2].slice(2).replace(',', '.'))
+        let dolarPrice = (formatedPrice * dolar).toFixed(2)
+        let product = {title:title.slice(1), preco:finalPrice[2], dolarPrice: '$' + dolarPrice, link, linkImg}
+        products.push(product)
+        })
+        console.log(products)
+    })
+    
+}
+
+
+keyboard()
+
+
+
+
+
+
